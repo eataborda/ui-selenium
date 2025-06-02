c52abf84910fa8b575839c601d2bd524f0f7d63c
Edwin Taborda <eataborda.m@gmail.com>
2025-06-01 20:05:52 -0500

    Update to trigger the updated tests

--- src/test/java/com/github/eataborda/ui/driver/WebDriverConfig.java
+++ src/test/java/com/github/eataborda/ui/driver/WebDriverConfig.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class WebDriverConfig {
